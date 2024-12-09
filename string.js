64ec283c2dd8463014ef628ce0189c41a1bb447a
ArbIsmajli <ismajliarb@gmail.com>
2024-12-09 18:42:19 +0100

    detyrapare

---
+++ string.js
@@ -0,0 +1,17 @@
+// let stringu1 = "ylli";
+// console.log(stringu1.length);
+// let indeksiifundit = stringu1.length - 1;
+// console.log(stringu1 [indeksiifundit]);
+
+let fjala = "abcd";
+// fjala[0] = a;
+// fjala[1] = b;
+// fjala[2] = c;
+// fjala[3] = d;
+// console.log(fjala.length);
+
+for (let i = 0; i< fjala.length; i++) {
+    console.log("fjala["+i +"] = "+ fjala[i]);
+}
+
+
